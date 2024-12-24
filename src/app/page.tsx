e1321fd31a1ba842984dd6dfeff9dc492cfe1850
ZubairAnwer <zubairmohomad4@gmail.com>
2024-12-24 18:23:58 +0500

    blog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import { FaAngleDoubleRight } from "react-icons/fa";
 export default function Home(){
     return(
         <>
-        <main className="max-w-screen-2xl mx-auto h-screen top-0 bg-custom3 bg-opacity-50 overflow-x-hidden">
-            <div className="flex flex-col-reverse lg:flex-row justify-between items-center pt-20">
+        <main className="max-w-screen-2xl mx-auto min-h-screen flex-none lg:flex items-center top-0 bg-custom3 bg-opacity-50 overflow-x-hidden">
+            <div className="flex flex-col-reverse lg:flex-row justify-between items-center pt-20 lg:pt-0">
                 <div className="w-[100%] lg:w-[50%] lg:px-16 mt-10 lg:mt-20">
                     <h1 className="text-center lg:text-left text-4xl sm:text-5xl lg:text-7xl font-extrabold text-black">Unveil New Perspectives</h1>
                     <p className="text-xl text-center lg:text-left py-5 lg:text-2xl lg:py-12 text-custom2 font-medium">Where every visit is an opportunity to uncover something new and inspiring.</p>
@@ -18,11 +18,7 @@ export default function Home(){
                     <Image className="px-3" src={"/images/home.jpg"} alt="home banner" width={600} height={600}/>
                 </div>
             </div>
-            <div className="pt-12 flex items-end justify-center">
-            <p className="text-center text-black  text-sm">
-                &copy;2024 Veil. | All Rights Reserved.
-            </p>
-        </div>
+           
         </main>
         </>
     )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,102 +11,100 @@ export default function Navbar() {
   return (
     <>
       <nav className="fixed w-full z-10">
-        <div className="max-w-screen-2xl mx-auto">
-          <div className="hidden md:block">
-            <div className="flex justify-between items-center px-10 py-4 text-amber-100 bg-custom2 bg-opacity-95">
-              <div>
-                <Link
-                  className="text-3xl font-bold font-serif hover:text-custom1"
-                  href={"/"}
-                >
-                  Veil.|<sub className="text-xs font-sans">a blog website</sub>
-                </Link>
-              </div>
-
-              <div className="flex space-x-8 text-xl px-10">
-                <Link
-                  href={"/"}
-                  className="hover:underline hover:underline-offset-4"
-                >
-                  Home
-                </Link>
-                <Link
-                  className="hover:underline hover:underline-offset-4"
-                  href={"/blog"}
-                >
-                  Blogs
-                </Link>
-                <Link
-                  className="hover:underline hover:underline-offset-4"
-                  href={"/about"}
-                >
-                  About
-                </Link>
-                <Link
-                  className="hover:underline hover:underline-offset-4"
-                  href={"/contact"}
-                >
-                  Contact Us
-                </Link>
-              </div>
-            </div>
-          </div>
-
-          {/* small screen hamburger menu bar */}
-          <div className="md:hidden">
-            <div className="flex justify-between items-center px-5 py-4 text-amber-100 bg-custom2 bg-opacity-95">
+        <div className="hidden md:block">
+          <div className="flex justify-between items-center px-10 py-4 text-amber-100 bg-custom2 bg-opacity-95">
+            <div>
               <Link
-                className="text-2xl sm:text-3xl font-bold font-serif hover:text-custom1"
+                className="text-3xl font-bold font-serif hover:text-custom1"
                 href={"/"}
               >
                 Veil.|<sub className="text-xs font-sans">a blog website</sub>
               </Link>
-              <Sheet>
-                <div className="flex items-center">
-                  <SheetTrigger>
-                    <MdMenuBook className="text-amber-100 w-7 h-7" />
-                  </SheetTrigger>
-                </div>
-                <SheetContent className="bg-custom2 bg-opacity-90 pt-20 text-center">
-                  {/* Add a hidden title for accessibility */}
-                  <SheetTitle className="sr-only">Menu</SheetTitle>
-                  <ul className="flex flex-col space-y-7">
-                    <li>
-                      <Link
-                        href={"/"}
-                        className="text-amber-100 text-3xl font-bold"
-                      >
-                        Home
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        className="text-amber-100 text-3xl font-bold"
-                        href="/blog"
-                      >
-                        Blogs
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        className="text-amber-100 text-3xl font-bold"
-                        href="/about"
-                      >
-                        About
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        className="text-amber-100 text-3xl font-bold"
-                        href={"/contact"}
-                      >
-                        Contact Us
-                      </Link>
-                    </li>
-                  </ul>
-                </SheetContent>
-              </Sheet>
             </div>
+
+            <div className="flex space-x-8 text-xl px-10">
+              <Link
+                href={"/"}
+                className="hover:underline hover:underline-offset-4"
+              >
+                Home
+              </Link>
+              <Link
+                className="hover:underline hover:underline-offset-4"
+                href={"/blog"}
+              >
+                Blogs
+              </Link>
+              <Link
+                className="hover:underline hover:underline-offset-4"
+                href={"/about"}
+              >
+                About
+              </Link>
+              <Link
+                className="hover:underline hover:underline-offset-4"
+                href={"/contact"}
+              >
+                Contact Us
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        {/* small screen hamburger menu bar */}
+        <div className="md:hidden">
+          <div className="flex justify-between items-center px-5 py-4 text-amber-100 bg-custom2 bg-opacity-95">
+            <Link
+              className="text-2xl sm:text-3xl font-bold font-serif hover:text-custom1"
+              href={"/"}
+            >
+              Veil.|<sub className="text-xs font-sans">a blog website</sub>
+            </Link>
+            <Sheet>
+              <div className="flex items-center">
+                <SheetTrigger>
+                  <MdMenuBook className="text-amber-100 w-7 h-7" />
+                </SheetTrigger>
+              </div>
+              <SheetContent className="bg-custom2 bg-opacity-90 pt-20 text-center">
+                {/* Add a hidden title for accessibility */}
+                <SheetTitle className="sr-only">Menu</SheetTitle>
+                <ul className="flex flex-col space-y-7">
+                  <li>
+                    <Link
+                      href={"/"}
+                      className="text-amber-100 text-3xl font-bold"
+                    >
+                      Home
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="text-amber-100 text-3xl font-bold"
+                      href="/blog"
+                    >
+                      Blogs
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="text-amber-100 text-3xl font-bold"
+                      href="/about"
+                    >
+                      About
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="text-amber-100 text-3xl font-bold"
+                      href={"/contact"}
+                    >
+                      Contact Us
+                    </Link>
+                  </li>
+                </ul>
+              </SheetContent>
+            </Sheet>
           </div>
         </div>
       </nav>
